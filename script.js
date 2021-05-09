38770446a17ad5b9d4316f4836c1f5336d685ff0
Victor Hüni <victor.hueni@protonmail.com>
2021-05-09 19:41:21 +0200

    fetching data

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('testing')
+/* Unsplash API */
+const count = 10;
+const apiKey = 'r2ui2UNBJfauzBe1T3AHg3nS__ICGl1JL-49zfv_guU';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+/* Get photos from unsplash API */
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        //Catch error her
+        console.log('whoops error during retrieving photos', error);
+    }
+}
+
+/* On load */
+getPhotos();
